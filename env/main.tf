66319b5f5cfe4fdddcc24757594a60c7e8ef6f93
Akash <akashbiond@gmail.com>
2020-08-02 17:26:34 +0530

    include iam and ec2 in main file

--- env/main.tf
+++ env/main.tf
@@ -19,3 +19,20 @@ module "vpc" {
   az_count = var.az_count
   cidr_block = var.cidr_block
 }
+
+module "iam_s3" {
+  source = "../resources/iam/s3"
+  env = var.env
+  s3_bucket = module.s3.bucket_arn
+}
+
+module "ec2" {
+  source = "../resources/ec2"
+  ami_id = var.ami_id
+  app_name = var.app_name
+  env = var.env
+  host_key = var.host_key
+  public_subnet_ids = module.vpc.public_subnet_ids
+  vpc_id = module.vpc.vpc_id
+  s3_role_name = module.iam_s3.s3_access_role_name
+}
